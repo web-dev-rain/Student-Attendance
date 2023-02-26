167acf1c0d5239e8c53fcee9a776bb929fbd7021
web-dev-rain <it.cloud.github@gmail.com>
2023-02-26 17:59:06 +0300

    add class StudentAdapter.java

---
+++ app/src/main/java/com/example/attendanceapp/StudentAdapter.java
@@ -0,0 +1,63 @@
+package com.example.attendanceapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+    ArrayList<StudentItem> studentItems;
+    Context context;
+
+    private OnItemClickListener onItemClickListener;
+
+    public interface OnItemClickListener {
+        void onClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    public StudentAdapter(Context context, ArrayList<StudentItem> studentItems) {
+        this.studentItems = studentItems;
+        this.context = context;
+    }
+
+    public static class StudentViewHolder extends RecyclerView.ViewHolder {
+        TextView roll;
+        TextView name;
+        TextView status;
+
+        public StudentViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
+            super(itemView);
+            roll = itemView.findViewById(R.id.roll);
+            name = itemView.findViewById(R.id.name);
+            itemView.setOnClickListener(v->onItemClickListener.onClick(getAdapterPosition()));
+        }
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_item, parent, false);
+        return new StudentViewHolder(itemView, onItemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, int position) {
+        holder.className.setText(classItems.get(position).getClassName());
+        holder.subjectName.setText(classItems.get(position).getSubjectName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return classItems.size();
+    }
+}
